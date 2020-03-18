975ec4a0a3eb9162a4c507f8d12861e45c4f6c71
sourav santra <souravsantra59580@gmail.com>
2020-03-18 11:48:48 +0530

    last commit(18/03/2020)

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const express = require("express");
 const socket = require("socket.io");
 const app = express();
 
-
+//Static file
+app.use(express.static('./public'));
 
 //Routes
 app.get("/",(req,res)=>{
-   res.sendfile("./index.html");
+   res.sendfile("index.html");
 })
 
 
 //Connection create
 const port = process.env.PORT || 3000;
 var server = app.listen(port,()=> console.log(`server is running at port ${port}`));
+
+//Socket connection
+var io = socket(server);
+
+io.on("connection",function(socket){
+  socket.on("chat",function(data){
+     io.sockets.emit("chat",data);
+  });
+
+  socket.on("typing",function(data){
+     socket.broadcast.emit("typing",data);
+  })
+})
